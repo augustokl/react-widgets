b2840bb111399fc393003e01b4e15d03697fbbb2
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-08-19 00:52:34 -0300

    add dropdown widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 
 const items = [
@@ -18,10 +19,35 @@ const items = [
   },
 ]
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  },
+]
+
 function App() {
+  const [selected, setSelected] = useState(options[0])
+  const [showDropdown, setShowDropdown] = useState(true)
+
   return (
     <div className="App" style={{margin: '30px 10px'}}>
-      <Search />
+      <button onClick={() => setShowDropdown(show => !show )}>Toggle Dropdown</button>
+      {showDropdown &&
+        <Dropdown 
+          selected={selected} 
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      }
     </div>
   );
 }
